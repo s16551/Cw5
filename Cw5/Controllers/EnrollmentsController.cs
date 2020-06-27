6514178034cdbdb65ae87b3e9d1fb74a20342a3c
Maciej Adrych <MaciejA@bonair.com.pl>
2020-06-27 12:59:54 +0200

    Dodanie EnrollmentsController

---
+++ Cw5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw5.Controllers
+{
+    [Route("api/[enrollments]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        public IActionResult EnrollStudent()
+        {
+            return Ok();
+        }
+    }
+}
